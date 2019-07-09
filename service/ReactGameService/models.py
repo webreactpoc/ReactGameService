ca0e6cac218ead2695174b9b290d37d0b8d43747
Marc-Antoine Dufresne Gagnon <mdufresnegagnon@gmail.com>
2019-07-09 14:05:02 -0400

    initial model

--- service/ReactGameService/models.py
+++ service/ReactGameService/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class testee(models.Model):
+    testee_id = models. #(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
 # Create your models here.
--- service/mysite/settings.py
+++ service/mysite/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
+DATABASES = { # TODO change to actual DB (MySQL?)
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
