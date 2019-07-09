0bd06e3659720e92474bf2936f23fb22cd9b2927
mdufresnegagnon@gmail.com <mdufresnegagnon@gmail.com>
2019-07-09 14:38:08 -0400

    simple data model design to start experimenting

--- service/ReactGameService/models.py
+++ service/ReactGameService/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 
-class testee(models.Model):
-    testee_id = models. #(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Test(models.Model):
+    name =  models.TextField(max_length=200)
 
-# Create your models here.
+class Testee(models.Model):
+    username =  models.TextField(max_length=200)
+    google_username =  models.TextField(max_length=200)
+
+class Organisation(models.Model):
+    name =  models.TextField(max_length=200)
+
+class Status(models.Model):
+    type = models.TextField(max_length=200)
+
+class Attempt(models.Model):
+    point_result =  models.TextField(max_length=200)
+    time_result =  models.DurationField()
+    test_id = models.ForeignKey("Test", on_update=models.CASCADE)
+    testee_id = models.ForeignKey("Testee", on_update=models.CASCADE)
+    organisation_id = models.ForeignKey("Organisation", on_update=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
